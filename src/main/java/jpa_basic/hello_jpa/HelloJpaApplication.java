b7c83870acbb4b54ec9497fe4ade309acc59c704
seasameoil <whdwlgus12@gmail.com>
2022-03-13 01:17:30 +0900

    Initial commit

---
+++ src/main/java/jpa_basic/hello_jpa/HelloJpaApplication.java
@@ -0,0 +1,13 @@
+package jpa_basic.hello_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJpaApplication.class, args);
+	}
+
+}
